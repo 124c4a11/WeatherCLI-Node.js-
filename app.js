40001b005580fb31c998b88cc69839e446a0cfe2
124c4a11 <124c4a11@mail.ru>
2021-12-26 20:26:13 +0300

    Saving the token in weather-data.json

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
 #!/usr/bin/env node
 
 import { getArgs } from './helpers/args.js';
-import { printHelp } from './services/log.service.js';
+import { printHelp, printSuccess, printErr } from './services/log.service.js';
+import { saveKeyValue } from './services/storage.service.js';
+
+
+async function saveToken(token) {
+  if (!token.length) {
+    printErr('Не передан token!');
+    return;
+  }
+
+  try {
+    await saveKeyValue('token', token);
+
+    printSuccess('Токен сохранен!');
+  } catch (err) {
+    printErr(err.message);
+  }
+}
 
 
 function InitCLI() {
@@ -9,6 +26,11 @@ function InitCLI() {
 
   if (args.h) {
     printHelp();
+    return;
+  }
+
+  if (args.t) {
+    saveToken(args.t);
     return;
   }
 }
---
+++ services/storage.service.js
@@ -0,0 +1,42 @@
+import { promises } from 'fs';
+import { join, resolve } from 'path';
+
+
+const filePath = join(resolve(), 'weather-data.json');
+
+
+async function isExist(path) {
+  try {
+    await promises.stat(path);
+    return true;
+  } catch (err) {
+    return false;
+  }
+}
+
+
+export async function saveKeyValue(key, value) {
+  let data = {};
+
+  if (await isExist(filePath)) {
+    const file = await promises.readFile(filePath);
+
+    data = JSON.parse(file);
+  }
+
+  data[key] = value;
+
+  await promises.writeFile(filePath, JSON.stringify(data));
+}
+
+
+export async function getKey(key) {
+  if (await isExist(filePath)) {
+    const file = await promises.readFile(filePath);
+    const data = JSON.parse(file);
+
+    return data[key]
+  }
+
+  return undefined;
+}
